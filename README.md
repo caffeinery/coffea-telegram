77f058371f079b0992616586014000111ad0b17a
Daniel Bugl <daniel.bugl@touchlay.com>
2015-08-03 13:57:28 -0400

    init coffea-telegram project

---
+++ README.md
@@ -0,0 +1,16 @@
+# coffea-telegram
+
+_telegram plugin for [coffea](https://github.com/caffeinery/coffea/)_
+
+
+## Setup
+
+ * Make sure to use the latest *unstable* version of coffea by specifying: `"coffea": "caffeinery/coffea#plugin-manager"` in your dependencies
+ * Install `coffea-telegram`: `npm install --save coffea-telegram`
+ * Load plugin: ```var client = require('coffea')();
+client.loadPlugin('coffea-telegram');```
+
+
+## Usage
+
+`coffea-telegram` aims to be compatible with coffea. Of course, features that telegram doesn't have (like joining channels) aren't available for telegram protocols and will result in an error if called on a telegram stream. (If `join` is called in the `motd` event it shouldn't make a difference though, as telegram doesn't emit that event)
---
+++ index.js
@@ -0,0 +1,68 @@
+/*jslint node: true*/
+"use strict";
+
+var debug = require('debug')('telegram');
+
+var TelegramBot = require('node-telegram-bot-api');
+
+module.exports = function (cb) {
+    return function (coffea) {
+        var utils = coffea.utils;
+
+        debug('loading protocol');
+
+        function T(network) {
+            return coffea.streams[network];
+        }
+
+        coffea.protocols.telegram = {
+
+            parse: function parseTelegram(config, shortConfig) {
+                if (shortConfig) config.token = shortConfig;
+                return config;
+            },
+
+            setup: function setupTelegram(config) {
+                debug('setting up stream');
+
+                var stream = new TelegramBot(config.token, {polling: true});
+
+                debug('setup stream');
+
+                return stream;
+            },
+
+            Parser: {}
+        }; // TODO: don't define parse and setup like this, maybe just use coffea.define('telegram', {functionname: function () {}})
+
+        coffea.define('telegram', 'connect', function connectTelegram(stream_id) {
+            debug('connecting to telegram');
+            var bot = coffea.streams[stream_id];
+
+            bot.on('message', function (msg) {
+                debug('recv %s', JSON.stringify(msg));
+                msg.from.nick = msg.from.username ? msg.from.username : msg.from.id; // alias
+                utils.emit(coffea, stream_id, 'message', {
+                    "channel": msg.chat.id !== msg.from.id ? msg.chat : undefined, // TODO: use Channel object
+                    "user": msg.from, // TODO: use User object
+                    "message": msg.text,
+                    "telegram": msg
+                    // "isAction": isAction,
+                    // "tags": msg.tags ? msg.tags : []
+                });
+            });
+        });
+
+        coffea.define('telegram', 'write', function writeTelegram() { });
+        // FIXME: this is just a workaround because the irc protocol still calls write to all networks sometimes and we don't handle "send to all networks" in the new system yet
+
+        coffea.define('telegram', 'send', function telegramSend(target, msg, network, fn) {
+            if (typeof target !== "string") target = target.id;
+            T(network).sendMessage(target, msg);
+        });
+
+        coffea.loadPlugins(__dirname + '/plugins/');
+        debug('protocol initialized');
+        if (cb) cb();
+    };
+};
