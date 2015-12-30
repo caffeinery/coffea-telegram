63194797705a748b414af544ad2b7613c61af3dc
Daniel Bugl <me@omnidan.net>
2015-12-30 23:14:57 +0100

    0.0.9: remove separate command handler, fixes #4

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ module.exports = function (cb) {
                 debug('setting up stream');
 
                 var stream = new TelegramBot(config.token, {polling: true});
+                config.prefix = '/';
 
                 debug('setup stream');
 
@@ -45,61 +46,9 @@ module.exports = function (cb) {
                 debug('user: %s', JSON.stringify(bot.me));
             });
 
-            function parseTelegramCommand(me, msg) {
-                var text = msg.text;
-                if (text.charAt(0) === '/') {
-                    debugC("command detected: %s", text);
-
-                    var doEmit = false;
-
-                    var message = coffea.splitString(text);
-                    debugC("message: %s", JSON.stringify(message));
-
-                    var cmdStr = message.shift();
-                    var cmdStrSplit = cmdStr.split('@');
-                    debugC("cmdStrSplit: %s", JSON.stringify(cmdStrSplit));
-                    var cmd = cmdStrSplit.shift();
-                    var username = cmdStrSplit.shift();
-
-                    debugC("cmdStr: %s - cmd: %s - username: %s - me: %s",
-                        JSON.stringify(cmdStr),
-                        JSON.stringify(cmd),
-                        JSON.stringify(username),
-                        JSON.stringify(me)
-                    );
-
-                    if (username) {
-                        if (username === me.username) {
-                            doEmit = true;
-                            var newText = text.split(' ');
-                            newText[0] = cmd; // replace cmdStr with cmd
-                            msg.text = newText.join(' '); // this also sets the text for on('message') events!
-                        }
-                    } else {
-                        doEmit = true;
-                    }
-
-                    debugC("doEmit: %s", doEmit);
-
-                    if (doEmit) {
-                        utils.emit(coffea, stream_id, 'command', {
-                            "channel": msg.chat.id !== msg.from.id ? msg.chat : undefined, // TODO: use Channel object
-                            "user": msg.from, // TODO: use User object
-                            "message": msg.text,
-                            "cmd": cmd.substr(1),
-                            "args": message,
-                            // "isAction": isAction,
-                            // "tags": msg.tags ? msg.tags : []
-                        });
-                    }
-                }
-                return text;
-            }
-
             bot.on('text', function (msg) {
                 debug('recv %s', JSON.stringify(msg));
                 msg.from.nick = msg.from.username ? msg.from.username : msg.from.id; // alias
-                parseTelegramCommand(bot.me, msg);
                 utils.emit(coffea, stream_id, 'message', {
                     "channel": msg.chat.id !== msg.from.id ? msg.chat : undefined, // TODO: use Channel object
                     "user": msg.from, // TODO: use User object
